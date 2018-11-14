2ac0e1f35faf1de2073bddd419e4e3fc30a46376
WChmura <wchmura28@gmail.com>
2018-11-14 18:56:58 +0100

    Update Main.py
    standardisation

--- Main.py
+++ Main.py
@@ -1,3 +1,12 @@
+import pandas as pd
 from sklearn.datasets import load_boston
+from sklearn.preprocessing import StandardScaler
+
 boston_market_data = load_boston()
-print(boston_market_data['DESCR'])
+#print(boston_market_data['DESCR'])
+#boston_market_data_p = pd.DataFrame(boston_market_data.data, columns=[boston_market_data.feature_names])
+#boston_market_data_p.head()
+#print(boston_market_data_p.describe())
+scaler = StandardScaler()
+boston_market_data['data'] = scaler.fit_transform(boston_market_data['data'])
+
